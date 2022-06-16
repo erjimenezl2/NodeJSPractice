9f0a917fcfa2146d9b2208091603b3dd152f7b24
Edgar Jimenez <edgjimen@publicisgroupe.net>
2022-06-15 21:05:55 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+const orderRoutes = require('./routes/order');
+
+// app
+const app = express();
+
+// db
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true        
+    })
+    .then(() => console.log('DB Connected'));
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+app.use('/api', orderRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
